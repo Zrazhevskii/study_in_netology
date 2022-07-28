542b4302a95efd922464bcd3995ac00106bb3030
Zrazhevskii Denis <vbn-134@yandex.ru>
2022-07-28 13:29:52 +0300

    add_homework8_task1

--- Task1,2.py
+++ Task1,2.py
@@ -1,28 +1,48 @@
+
+#--------------------------------ЗАДАНИЕ1-------------------------------------
 from pprint import pprint
+import requests
 
-def read_cook_book(file):
-    data = {}
-    key = ['ingredient_name', 'quantity', 'measure']
-    with open(file, 'r', encoding='utf-8') as f:
-        while True:
-            ingredients = []
-            name = f.readline().rstrip()
-            if not name:
-                break
-            ingredient_count = f.readline().rstrip()
-            for i in range(int(ingredient_count)):
-                ing = f.readline().rstrip()
-                ing_list = ing.strip().split("|")
-                ingredient = dict(zip(key, ing_list))
-                ingredient['quantity'] = int(ingredient['quantity'])
-                ingredients.append(ingredient)
-            data[name] = ingredients
-            f.readline().rstrip()
-    return data
+TOKEN = '2619421814940190'
+urls = [
+    f'https://www.superheroapi.com/api.php/{TOKEN}/search/Hulk',
+    f'https://www.superheroapi.com/api.php/{TOKEN}/search/Thanos',
+    f'https://www.superheroapi.com/api.php/{TOKEN}/search/Captain%America',
+]  # список адресов
 
-file = 'data.txt'
-data = read_cook_book(file)
-pprint(data)
+
+def requests_get(url_all):
+    # принимает список адресов
+    r = (requests.get(url) for url in url_all)
+    return r
+
+
+def parser():
+    # функция парсинга интелекта
+    super_man = []
+    for item in requests_get(urls):
+        intelligence = item.json()
+        try:
+            for power_stats in intelligence['results']:
+                super_man.append({
+                    'name': power_stats['name'],
+                    'intelligence': power_stats['powerstats']['intelligence'],
+                })
+        except KeyError:
+            print(f"Проверте ссылки urls: {urls}")
+
+    intelligence_super_hero = 0
+    name = ''
+    for intelligence_hero in super_man:
+        if intelligence_super_hero < int(intelligence_hero['intelligence']):
+            intelligence_super_hero = int(intelligence_hero['intelligence'])
+            name = intelligence_hero['name']
+
+    print(f"Самый интелектуальный {name}, интелект: {intelligence_super_hero}")
+
+
+parser()
+
 
 #---------------------------- ЗАДАЧА 2 -------------------------------------
 
